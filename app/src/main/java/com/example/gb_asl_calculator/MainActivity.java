c1005cc6540a2e809436ceb7b84ae3185c13dbf9
kos_ykt <kosvasl09@gamil.com>
2021-05-28 21:47:31 +0900

    setOnClickListener

--- app/src/main/java/com/example/gb_asl_calculator/MainActivity.java
+++ app/src/main/java/com/example/gb_asl_calculator/MainActivity.java
@@ -3,37 +3,31 @@ package com.example.gb_asl_calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int counter = 0;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
+        textView = findViewById(R.id.calculator_txt);
+        Button button = findViewById(R.id.btn_0);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
+                textView.setText(String.format(Locale.getDefault(), "%d", counter));
+            }
+        });
     }
 }
