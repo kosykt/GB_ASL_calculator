6dc0134d3788ec1359e7839ed3100a2651bee82e
kos_ykt <kosvasl09@gamil.com>
2021-05-28 21:58:44 +0900

    add methods

--- app/src/main/java/com/example/gb_asl_calculator/MainActivity.java
+++ app/src/main/java/com/example/gb_asl_calculator/MainActivity.java
@@ -12,22 +12,30 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
 
     private int counter = 0;
-    private TextView textView;
+    private TextView calculatorView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        initView();
+    }
 
-        textView = findViewById(R.id.calculator_txt);
-        Button button = findViewById(R.id.btn_0);
-        button.setOnClickListener(new View.OnClickListener() {
+    private void initView() {
+        calculatorView = findViewById(R.id.calculator_txt);
+        Button button_0 = findViewById(R.id.btn_0);
+        button_0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 counter++;
+                setTextOnCalculatorView(calculatorView, counter);
 
-                textView.setText(String.format(Locale.getDefault(), "%d", counter));
             }
         });
+    }
+
+    private void setTextOnCalculatorView(TextView view, int num) {
+        view.setText(String.format(Locale.getDefault(), "%d", num));
     }
 }
