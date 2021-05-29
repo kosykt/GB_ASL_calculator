ecd6f5089a17eeb320c20e9e2146754e4f36e63f
kos_ykt <kosvasl09@gamil.com>
2021-05-29 21:48:42 +0900

    init buttons

--- app/src/main/java/com/example/gb_asl_calculator/MainActivity.java
+++ app/src/main/java/com/example/gb_asl_calculator/MainActivity.java
@@ -27,11 +27,96 @@ public class MainActivity extends AppCompatActivity {
 
     private void initView() {
         calculatorView = findViewById(R.id.calculator_txt);
+
+        initButton_0();
+        initButton_1();
+        initButton_2();
+        initButton_3();
+        initButton_4();
+        initButton_5();
+        initButton_6();
+        initButton_7();
+        initButton_8();
+        initButton_9();
+    }
+
+    private void initButton_9() {
+        Button button_9 = findViewById(R.id.btn_9);
+        button_9.setOnClickListener(v -> {
+            counter.incrementCounter();
+            MainActivity.this.setTextOnCalculatorView(calculatorView, counter.getCounter());
+        });
+    }
+
+    private void initButton_8() {
+        Button button_8 = findViewById(R.id.btn_8);
+        button_8.setOnClickListener(v -> {
+            counter.incrementCounter();
+            MainActivity.this.setTextOnCalculatorView(calculatorView, counter.getCounter());
+        });
+    }
+
+    private void initButton_7() {
+        Button button_7 = findViewById(R.id.btn_7);
+        button_7.setOnClickListener(v -> {
+            counter.incrementCounter();
+            MainActivity.this.setTextOnCalculatorView(calculatorView, counter.getCounter());
+        });
+    }
+
+    private void initButton_6() {
+        Button button_6 = findViewById(R.id.btn_6);
+        button_6.setOnClickListener(v -> {
+            counter.incrementCounter();
+            MainActivity.this.setTextOnCalculatorView(calculatorView, counter.getCounter());
+        });
+    }
+
+    private void initButton_5() {
+        Button button_5 = findViewById(R.id.btn_5);
+        button_5.setOnClickListener(v -> {
+            counter.incrementCounter();
+            MainActivity.this.setTextOnCalculatorView(calculatorView, counter.getCounter());
+        });
+    }
+
+    private void initButton_4() {
+        Button button_4 = findViewById(R.id.btn_4);
+        button_4.setOnClickListener(v -> {
+            counter.incrementCounter();
+            MainActivity.this.setTextOnCalculatorView(calculatorView, counter.getCounter());
+        });
+    }
+
+    private void initButton_3() {
+        Button button_3 = findViewById(R.id.btn_3);
+        button_3.setOnClickListener(v -> {
+            counter.incrementCounter();
+            MainActivity.this.setTextOnCalculatorView(calculatorView, counter.getCounter());
+        });
+    }
+
+    private void initButton_2() {
+        Button button_2 = findViewById(R.id.btn_2);
+        button_2.setOnClickListener(v -> {
+            counter.incrementCounter();
+            MainActivity.this.setTextOnCalculatorView(calculatorView, counter.getCounter());
+        });
+    }
+
+    private void initButton_1() {
+        Button button_1 = findViewById(R.id.btn_1);
+        button_1.setOnClickListener(v -> {
+            counter.incrementCounter();
+            MainActivity.this.setTextOnCalculatorView(calculatorView, counter.getCounter());
+        });
+    }
+
+    private void initButton_0() {
         Button button_0 = findViewById(R.id.btn_0);
         button_0.setOnClickListener(v -> {
             counter.incrementCounter();
-            setTextOnCalculatorView(calculatorView, counter.getCounter());
-
+            MainActivity.this.setTextOnCalculatorView(calculatorView, counter.getCounter());
         });
     }
 
