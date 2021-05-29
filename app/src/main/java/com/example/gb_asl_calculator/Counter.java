cd6361d836a4d939158d31e641b0687bf8f7d264
kos_ykt <kosvasl09@gamil.com>
2021-05-29 21:27:26 +0900

    implements Parcelable

--- app/src/main/java/com/example/gb_asl_calculator/Counter.java
+++ app/src/main/java/com/example/gb_asl_calculator/Counter.java
@@ -1,17 +1,46 @@
 package com.example.gb_asl_calculator;
 
-public class Counter {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Counter implements Parcelable {
     private int counter;
 
-    public Counter(){
+    public Counter() {
         counter = 0;
     }
+
+    protected Counter(Parcel in) {
+        counter = in.readInt();
+    }
+
+    public static final Creator<Counter> CREATOR = new Creator<Counter>() {
+        @Override
+        public Counter createFromParcel(Parcel in) {
+            return new Counter(in);
+        }
+
+        @Override
+        public Counter[] newArray(int size) {
+            return new Counter[size];
+        }
+    };
 
     public int getCounter() {
         return counter;
     }
 
-    public void incrementCounter(){
+    public void incrementCounter() {
         counter++;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(counter);
     }
 }
--- app/src/main/java/com/example/gb_asl_calculator/MainActivity.java
+++ app/src/main/java/com/example/gb_asl_calculator/MainActivity.java
@@ -1,15 +1,17 @@
 package com.example.gb_asl_calculator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String keyCounter = "Counter";
 
     private Counter counter;
     private TextView calculatorView;
@@ -26,17 +28,31 @@ public class MainActivity extends AppCompatActivity {
     private void initView() {
         calculatorView = findViewById(R.id.calculator_txt);
         Button button_0 = findViewById(R.id.btn_0);
-        button_0.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                counter.incrementCounter();
-                setTextOnCalculatorView(calculatorView, counter.getCounter());
+        button_0.setOnClickListener(v -> {
+            counter.incrementCounter();
+            setTextOnCalculatorView(calculatorView, counter.getCounter());
 
-            }
         });
     }
 
     private void setTextOnCalculatorView(TextView view, int num) {
         view.setText(String.format(Locale.getDefault(), "%d", num));
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle instanceState) {
+        super.onSaveInstanceState(instanceState);
+        instanceState.putParcelable(keyCounter, counter);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle instanceState) {
+        super.onRestoreInstanceState(instanceState);
+        counter = instanceState.getParcelable(keyCounter);
+        setTextCounter();
+    }
+
+    private void setTextCounter() {
+        setTextOnCalculatorView(calculatorView, counter.getCounter());
     }
 }
