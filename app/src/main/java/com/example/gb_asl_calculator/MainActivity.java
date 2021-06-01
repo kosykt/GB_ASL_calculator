20ff7e87576de066c4032d562691fb3e3decb5e9
kos_ykt <kosvasl09@gamil.com>
2021-06-02 01:39:39 +0900

    add subText

--- app/src/main/java/com/example/gb_asl_calculator/MainActivity.java
+++ app/src/main/java/com/example/gb_asl_calculator/MainActivity.java
@@ -12,8 +12,8 @@ public class MainActivity extends AppCompatActivity {
 
     private Operation operation;
 
-
     private TextView calculatorView, saveNumView;
+    private String subText;
     private Button button_0, button_1, button_2, button_3, button_4, button_5, button_6, button_7, button_8, button_9, button_dot, button_plus, button_minus, button_divide, button_multiply, button_clear, button_result;
 //    private final Button[] numButtonsArr = new Button[]{button_0, button_1, button_2, button_3, button_4, button_5, button_6, button_7, button_8, button_9};
 //    private final int[] numberButtonIds = new int[]{R.id.btn_0, R.id.btn_1, R.id.btn_2, R.id.btn_3, R.id.btn_4, R.id.btn_5, R.id.btn_6, R.id.btn_7, R.id.btn_8, R.id.btn_9};
@@ -71,6 +71,7 @@ public class MainActivity extends AppCompatActivity {
         button_clear.setOnClickListener(v -> {
             saveNumView.setText(null);
             calculatorView.setText(null);
+            subText = null;
             operation.setSymbol(null);
             operation.setNum1(null);
             operation.setNum2(null);
@@ -82,57 +83,61 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void setNumInOperationClass() {
+    @SuppressLint("SetTextI18n")
+    private void setNumInOperationClass(String s) {
         if (operation.getClickMathOperation().equals(false)) {
+            calculatorView.setText(calculatorView.getText() + s);
             operation.setNum1((String) calculatorView.getText());
         } else {
-            //TODO ошибка начинается здесь
-            operation.setNum2("2");
+            calculatorView.setText(calculatorView.getText() + s);
+            if (subText == null){
+                subText = s;
+            }else {
+                subText = subText + s;
+            }
+            operation.setNum2(subText);
         }
     }
 
-    @SuppressLint("SetTextI18n")
     private void setNumOnView() {
         button_0.setOnClickListener(v -> {
-            calculatorView.setText(calculatorView.getText() + "0");
-            setNumInOperationClass();
+            setNumInOperationClass("0");
         });
         button_1.setOnClickListener(v -> {
-            calculatorView.setText(calculatorView.getText() + "1");
-            setNumInOperationClass();
+            setNumInOperationClass("1");
         });
-        button_2.setOnClickListener(v -> {
-            calculatorView.setText(calculatorView.getText() + "2");
-            setNumInOperationClass();
-        });
-        button_3.setOnClickListener(v -> {
-            calculatorView.setText(calculatorView.getText() + "3");
-            setNumInOperationClass();
-        });
-        button_4.setOnClickListener(v -> {
-            calculatorView.setText(calculatorView.getText() + "4");
-            setNumInOperationClass();
-        });
-        button_5.setOnClickListener(v -> {
-            calculatorView.setText(calculatorView.getText() + "5");
-            setNumInOperationClass();
-        });
-        button_6.setOnClickListener(v -> {
-            calculatorView.setText(calculatorView.getText() + "6");
-            setNumInOperationClass();
-        });
-        button_7.setOnClickListener(v -> {
-            calculatorView.setText(calculatorView.getText() + "7");
-            setNumInOperationClass();
-        });
-        button_8.setOnClickListener(v -> {
-            calculatorView.setText(calculatorView.getText() + "8");
-            setNumInOperationClass();
-        });
-        button_9.setOnClickListener(v -> {
-            calculatorView.setText(calculatorView.getText() + "9");
-            setNumInOperationClass();
-        });
+//        button_2.setOnClickListener(v -> {
+//            calculatorView.setText(calculatorView.getText() + "2");
+//            setNumInOperationClass();
+//        });
+//        button_3.setOnClickListener(v -> {
+//            calculatorView.setText(calculatorView.getText() + "3");
+//            setNumInOperationClass();
+//        });
+//        button_4.setOnClickListener(v -> {
+//            calculatorView.setText(calculatorView.getText() + "4");
+//            setNumInOperationClass();
+//        });
+//        button_5.setOnClickListener(v -> {
+//            calculatorView.setText(calculatorView.getText() + "5");
+//            setNumInOperationClass();
+//        });
+//        button_6.setOnClickListener(v -> {
+//            calculatorView.setText(calculatorView.getText() + "6");
+//            setNumInOperationClass();
+//        });
+//        button_7.setOnClickListener(v -> {
+//            calculatorView.setText(calculatorView.getText() + "7");
+//            setNumInOperationClass();
+//        });
+//        button_8.setOnClickListener(v -> {
+//            calculatorView.setText(calculatorView.getText() + "8");
+//            setNumInOperationClass();
+//        });
+//        button_9.setOnClickListener(v -> {
+//            calculatorView.setText(calculatorView.getText() + "9");
+//            setNumInOperationClass();
+//        });
     }
 
     private void initCalculator() {
