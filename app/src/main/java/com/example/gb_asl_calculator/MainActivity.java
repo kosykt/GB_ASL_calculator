cea3d79667a8cab89fae47addcad29cfca6dc0ee
kos_ykt <kosvasl09@gamil.com>
2021-06-04 12:48:44 +0900

    FLAG_FULLSCREEN and NoActionBar

--- app/src/main/java/com/example/gb_asl_calculator/MainActivity.java
+++ app/src/main/java/com/example/gb_asl_calculator/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.gb_asl_calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -18,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         operation = new Operation();
 
